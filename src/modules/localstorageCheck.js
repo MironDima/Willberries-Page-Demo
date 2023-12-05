d1e2c94a9e02264a7cd42685568002f4081cdba7
Miron Dima <mirontsov2098@mail.ru>
2023-12-05 18:10:59 +0300

    add filter, search,viewall

---
+++ src/modules/localstorageCheck.js
@@ -0,0 +1,8 @@
+import { renderArrayData } from "./renderArrayData";
+const localstorageCheck = () => {
+	if(localStorage.getItem('array') && window.location.pathname === '/goods.html' ) {
+		renderArrayData(JSON.parse(localStorage.getItem('array')));
+	}
+}
+
+export {localstorageCheck}
